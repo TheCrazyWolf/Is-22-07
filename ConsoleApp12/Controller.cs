4df14e462896ff84d158ad06ef6b2acc0f6f850d
СГК ГАПОУ <sgk.gapou@outlook.com>
2022-11-30 09:33:23 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp12/Controller.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Controller
+    {
+        public static List<Good> Goods = new List<Good>();
+
+        public void AddGood(string title, float price, float sale, string desc)
+        {
+            Good good = new Good()
+            {
+                Title = title,
+                Price = price,
+                Sale = sale,
+                Description = desc
+            };
+
+            Goods.Add(good);
+        }
+
+        public string GetGoods()
+        {
+            string text = "";
+
+            foreach (var item in Goods)
+            {
+                text += $"{item.Title} цена - {item.Price}\n";
+            }
+
+            return text;
+        }
+    }
+}
---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,36 @@
+﻿using ConsoleApp12;
+using static System.Console;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        WriteLine("Hello, World!");
+
+
+        WriteLine("Выберите команду");
+
+
+        while (true)
+        {
+            var command = Console.ReadLine().ToLower().Split(' ');
+            Controller s = new Controller();
+            switch (command[0])
+            {
+
+                case "добавить":
+                    s.AddGood(command[1], float.Parse(command[2]), float.Parse(command[3]), command[4]);
+                    break;
+                case "список":
+                    Console.WriteLine(s.GetGoods());
+                    break;
+
+                default:
+                    WriteLine("Ошибка в комане");
+                    break;
+
+            }
+        }
+
+    }
+}
